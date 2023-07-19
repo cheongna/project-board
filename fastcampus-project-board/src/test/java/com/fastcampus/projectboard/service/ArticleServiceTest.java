77c935e663c52c053481439b67a60ed910f9e775
cheona <ehgus97334@gmail.com>
2023-07-20 06:06:26 +0900

    #14 - 게시판 서비스 기능 구현 기초 작성
    service controller 생성 및 테스트 작성

---
+++ fastcampus-project-board/src/test/java/com/fastcampus/projectboard/service/ArticleServiceTest.java
@@ -0,0 +1,35 @@
+package com.fastcampus.projectboard.service;
+
+import com.fastcampus.projectboard.domain.type.SearchType;
+import com.fastcampus.projectboard.dto.ArticleDto;
+import com.fastcampus.projectboard.repository.ArticleRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@DisplayName("비즈니스 로직 - 게시글")
+@ExtendWith(MockitoExtension.class)
+class ArticleServiceTest {
+    @InjectMocks
+    private ArticleService sut;
+    @Mock
+    private ArticleRepository articleRepository;
+
+    @DisplayName("게시글을 검색하면, 게시글 리스트를 반환")
+    @Test
+    public void givenSearchParameters_whenSearchingArticles_thenReturnArticlesList() {
+        Page<ArticleDto> articles = sut.searchArticles(SearchType.TITLE, "search keyword"); //제목, 본문, id, 닉네임, 해시태그
+    }
+
+    @DisplayName("게시글을 조회하면, 게스글을 반환한다. 단일")
+    @Test
+    public void givenArticleId_whenSearchingArticle_thenReturnArticle() {
+        ArticleDto article = sut.searchArticle(1L); //제목, 본문, id, 닉네임, 해시태그
+    }
+}
